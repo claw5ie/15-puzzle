4da3466405eda89054b399312ecf2daab6e26bac
claw5ie <92851066+claw5ie@users.noreply.github.com>
2022-03-18 20:18:54 +0000

    Add some stats to search result

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 #include <vector>
 #include <stack>
 #include <queue>
+#include <list>
 #include <set>
 #include <memory>
 #include <functional>
+#include <chrono>
 #include <cassert>
+
+#define MEMORY_LIMIT ((size_t)1024 * 1024)
 
 enum Direction
 {
@@ -45,10 +49,15 @@ Direction oposite(Direction direction)
 
 using Path = std::vector<Direction>;
 
+auto const chrono_default_value = std::chrono::duration<double>();
+
 struct SearchResult
 {
   bool found_path;
   Path path;
+  size_t explored_nodes_count;
+  size_t max_nodes_in_stack;
+  std::chrono::duration<double> time_spent;
 };
 
 #define EMPTY_SPACE 15
@@ -188,8 +197,8 @@ SearchResult depth_search_no_cycles(Board const &source, Board const &target)
     Path path;
   };
 
-  std::vector<Board> explored;
-  std::vector<Node> to_explore;
+  std::list<Board> explored;
+  std::list<Node> to_explore;
 
   auto const it_repeats_or_was_explored =
     [&to_explore, &explored](Board const &board) -> bool
@@ -210,13 +219,19 @@ SearchResult depth_search_no_cycles(Board const &source, Board const &target)
     };
 
   to_explore.push_back({ source, { } });
+  size_t max_nodes_in_stack = 1;
 
   while (!to_explore.empty())
   {
+    max_nodes_in_stack = std::max(max_nodes_in_stack, to_explore.size());
+
     Node node = std::move(to_explore.back());
 
     if (node.board.is_equal(target))
-      return { true, node.path };
+      return { true, std::move(node.path), explored.size(), max_nodes_in_stack, chrono_default_value };
+
+    if (to_explore.size() * sizeof(Node) > MEMORY_LIMIT)
+      return { false, { }, explored.size(), max_nodes_in_stack, chrono_default_value };
 
     explored.push_back(node.board);
     to_explore.pop_back();
@@ -240,7 +255,7 @@ SearchResult depth_search_no_cycles(Board const &source, Board const &target)
     }
   }
 
-  return { false, { } };
+  return { false, { }, explored.size(), max_nodes_in_stack, chrono_default_value };
 }
 
 SearchResult iterative_deepening_search(
@@ -260,15 +275,22 @@ SearchResult iterative_deepening_search(
   auto const iterative_deepening_search_aux =
     [&to_explore, &source, &target](size_t depth) -> SearchResult
     {
+      size_t explored_nodes_count = 0,
+        max_nodes_in_stack = 1;
+
       to_explore.push({ 0, source, { } });
 
       while (!to_explore.empty())
       {
+        max_nodes_in_stack = std::max(max_nodes_in_stack, to_explore.size());
+
         Node node = std::move(to_explore.top());
+
         to_explore.pop();
+        explored_nodes_count++;
 
         if (node.board.is_equal(target))
-          return { true, std::move(node.path) };
+          return { true, std::move(node.path), explored_nodes_count, max_nodes_in_stack, chrono_default_value };
         else if (node.depth > depth)
           continue;
 
@@ -288,18 +310,25 @@ SearchResult iterative_deepening_search(
         }
       }
 
-      return { false, { } };
+      return { false, { }, explored_nodes_count, max_nodes_in_stack, chrono_default_value };
     };
 
+  size_t max_nodes_in_stack = 0,
+    explored_nodes_count = 0;
+
+  // Assuming that the stack always grows when the depth increases.
   for (size_t depth = 0; depth <= max_depth; depth++)
   {
     auto res = iterative_deepening_search_aux(depth);
+
+    max_nodes_in_stack = std::max(max_nodes_in_stack, res.max_nodes_in_stack);
+    explored_nodes_count = std::max(explored_nodes_count, res.explored_nodes_count);
 
     if (res.found_path)
       return res;
   }
 
-  return { false, { } };
+  return { false, { }, max_nodes_in_stack, explored_nodes_count, chrono_default_value };
 }
 
 SearchResult breath_search(Board const &source, Board const &target)
@@ -314,13 +343,20 @@ SearchResult breath_search(Board const &source, Board const &target)
 
   to_explore.push({ source, { } });
 
+  size_t explored_nodes_count = 0,
+    max_nodes_in_stack = 1;
+
   while (!to_explore.empty())
   {
+    max_nodes_in_stack = std::max(max_nodes_in_stack, to_explore.size());
+
     Node node = std::move(to_explore.front());
+
     to_explore.pop();
+    explored_nodes_count++;
 
     if (node.board.is_equal(target))
-      return { true, std::move(node.path) };
+      return { true, std::move(node.path), max_nodes_in_stack, explored_nodes_count, chrono_default_value };
 
     for (int i = 0; i < Direction::COUNT; i++)
     {
@@ -338,7 +374,7 @@ SearchResult breath_search(Board const &source, Board const &target)
     }
   }
 
-  return { false, { } };
+  return { false, { }, max_nodes_in_stack, explored_nodes_count, chrono_default_value };
 }
 
 using HeuristicFunc = size_t (*)(Board const &, Board const &);
@@ -363,14 +399,20 @@ SearchResult a_star_search(Board const &source, Board const &target, HeuristicFu
       ));
 
   to_explore.insert({ source, { } });
+  size_t explored_nodes_count = 0,
+    max_nodes_in_stack = 1;
 
   while (!to_explore.empty())
   {
+    max_nodes_in_stack = std::max(max_nodes_in_stack, to_explore.size());
+
     Node node = std::move(*to_explore.begin());
+
     to_explore.erase(to_explore.begin());
+    explored_nodes_count++;
 
     if (node.board.is_equal(target))
-      return { true, std::move(node.path) };
+      return { true, std::move(node.path), max_nodes_in_stack, explored_nodes_count, chrono_default_value };
 
     for (int i = 0; i < Direction::COUNT; i++)
     {
@@ -389,10 +431,10 @@ SearchResult a_star_search(Board const &source, Board const &target, HeuristicFu
     }
   }
 
-  return { false, { } };
+  return { false, { }, max_nodes_in_stack, explored_nodes_count, chrono_default_value };
 }
 
-SearchResult greedy_search_with_heuristics(
+SearchResult greedy_search(
   Board const &source, Board const &target, HeuristicFunc heuristic
   )
 {
@@ -414,13 +456,23 @@ SearchResult greedy_search_with_heuristics(
 
   to_explore.insert({ source, { } });
 
+  size_t explored_nodes_count = 0,
+    max_nodes_in_stack = 1;
+
   while (!to_explore.empty())
   {
+    max_nodes_in_stack = std::max(max_nodes_in_stack, to_explore.size());
+
     Node node = std::move(*to_explore.begin());
+
     to_explore.erase(to_explore.begin());
+    explored_nodes_count++;
 
     if (node.board.is_equal(target))
-      return { true, std::move(node.path) };
+      return { true, std::move(node.path), max_nodes_in_stack, explored_nodes_count, chrono_default_value };
+
+    if (to_explore.size() > MEMORY_LIMIT)
+      return { false, { }, max_nodes_in_stack, explored_nodes_count, chrono_default_value };
 
     for (int i = 0; i < Direction::COUNT; i++)
     {
@@ -439,32 +491,128 @@ SearchResult greedy_search_with_heuristics(
     }
   }
 
-  return { false, { } };
+  return { false, { }, max_nodes_in_stack, explored_nodes_count, chrono_default_value };
+}
+
+enum SearchAlgorithm
+{
+  BREATH_FIRST,
+  DEPTH_NO_CYCLES_FIRST,
+  ITERATIVE_DEEPENING,
+  A_STAR_MANHATTAN,
+  A_STAR_PIECES_OUT_OF_PLACE,
+  GREEDY_MANHATTAN,
+  GREEDY_PIECES_OUT_OF_PLACE,
+  ALGORITHM_COUNT,
+};
+
+char const *to_string(SearchAlgorithm algorithm)
+{
+  static char const *const lookup[] = {
+    "Breath First",
+    "Depth First (With cycle detection)",
+    "Iterative Deepening",
+    "A* (Manhattan Distance)",
+    "A* (Number Of Pieces Out Of Place)",
+    "Greedy (Manhattan Distance)",
+    "Greedy (Number Of Pieces Out Of Place)"
+  };
+
+  return algorithm < ALGORITHM_COUNT ? lookup[algorithm] : nullptr;
+}
+
+#define MAX_DEPTH 32
+
+SearchResult choose_algorithm(
+  SearchAlgorithm algorithm, Board const &source, Board const &target
+  )
+{
+  if (!source.can_be_solved(target))
+    return { false, { }, 0, 0, chrono_default_value };
+
+  SearchResult res;
+  auto const start = std::chrono::steady_clock::now();
+
+  switch (algorithm)
+  {
+  case BREATH_FIRST:
+    res = breath_search(source, target);
+    break;
+  case DEPTH_NO_CYCLES_FIRST:
+    res = depth_search_no_cycles(source, target);
+    break;
+  case ITERATIVE_DEEPENING:
+    res = iterative_deepening_search(source, target, MAX_DEPTH);
+    break;
+  case A_STAR_MANHATTAN:
+    res = a_star_search(source, target, sum_of_manhattan_distances);
+    break;
+  case A_STAR_PIECES_OUT_OF_PLACE:
+    res = a_star_search(source, target, count_pieces_out_of_place);
+    break;
+  case GREEDY_MANHATTAN:
+    res = greedy_search(source, target, sum_of_manhattan_distances);
+    break;
+  case GREEDY_PIECES_OUT_OF_PLACE:
+    res = greedy_search(source, target, count_pieces_out_of_place);
+    break;
+  default:
+    assert(false);
+  }
+
+  res.time_spent = std::chrono::steady_clock::now() - start;
+
+  return res;
 }
 
 int main()
 {
-  Board source = { 0, { 0, 1, 2, 3, 4, 5, 6, 7, 8, 15, 10, 11, 12, 9, 13, 14 }};
+  Board sources[] = {
+    { 0, { 0, 1, 2, 3, 4, 5, 6, 7, 8, 15, 10, 11, 12, 9, 13, 14 }},
+    { 0, { 0, 1, 2, 3, 4, 5, 7, 11, 12, 8, 15, 6, 13, 10, 9, 14 }},
+    { 0, { 0, 1, 2, 3, 12, 5, 7, 11, 4, 8, 15, 6, 13, 10, 9, 14 }}
+  };
+
   Board target = { 15, { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }};
 
-  source.set_pos();
+  for (size_t i = 0; i < sizeof(sources) / sizeof(Board); i++)
+    sources[i].set_pos();
+
   target.set_pos();
 
-  std::cout << source.can_be_solved(target) <<
-    '\n' << source.is_equal(target) << '\n';
-
-  auto res = a_star_search(source, target, sum_of_manhattan_distances);
-
-  if (res.found_path)
+  for (size_t i = 0; i < sizeof(sources) / sizeof(Board); i++)
   {
-    size_t i = 0;
-    for (auto &dir: res.path)
+    std::cout << "\n\n";
+
+    for (int j = 0; j < ALGORITHM_COUNT; j++)
     {
-      i++;
-      std::cout << to_string(dir) << (i % 8 == 0 ? '\n' : ' ');
+      std::cout << "Chosen algorithm: " << to_string((SearchAlgorithm)j) << '\n';
+
+      auto res = choose_algorithm((SearchAlgorithm)j, sources[i], target);
+
+      if (res.found_path)
+      {
+        std::cout << " * Found solution:  ";
+
+        size_t i = 0;
+        for (auto &dir: res.path)
+        {
+          std::cout << to_string(dir);
+
+          if (++i < res.path.size())
+            std::cout << ' ';
+        }
+
+      }
+      else
+      {
+        std::cout << " * Didn't find anything :(";
+      }
+
+      std::cout << "\n   - Spent          " << res.time_spent.count() << " seconds\n"
+                << "   - Took           " << res.path.size() << " moves\n"
+                << "   - Explored       " << res.explored_nodes_count << " nodes in total\n"
+                << "   - Stored at most " << res.max_nodes_in_stack <<  " nodes in stack\n";
     }
   }
-
-  for (int i = 0; i < Direction::COUNT; i++)
-    std::cout << source.can_move((Direction)i) << '\n';
 }
